c7a305e67723e3fb8d945648be4167a8f95db7e3
Sadman-Sakib-AIUB <sadmansakib.aiub@gmail.com>
2025-11-21 18:28:37 +0600

    fly to location

--- src/Pages/Coverage/Coverage.jsx
+++ src/Pages/Coverage/Coverage.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useLoaderData } from "react-router";
 const Coverage = () => {
   const position = [23.685, 90.3563];
   const serviceCenters = useLoaderData();
-//   console.log(serviceCenters);
+  const mapRef = useRef(null);
+  //   console.log(serviceCenters);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const location = e.target.location.value;
+    const district = serviceCenters.find((center) =>
+      center.district.toLowerCase().includes(location.toLowerCase())
+    );
+
+    if (district) {
+      const coord = [district.latitude, district.longitude];
+    //   console.log(coord);
+
+    // fly to the location
+    mapRef.current.flyTo(coord,11);
+    }
+    
+  };
 
   return (
     <div>
@@ -13,12 +32,43 @@ const Coverage = () => {
         We are available in 64 districts
       </h2>
 
+      {/* Search box */}
+      <div className="my-5">
+        <form onSubmit={handleSubmit}>
+          <label className="input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input
+              type="search"
+              name="location"
+              className="grow"
+              placeholder="Search"
+            />
+          </label>
+        </form>
+      </div>
+
       <div>
         <MapContainer
           center={position}
           zoom={7}
           scrollWheelZoom={false}
           className="w-full h-[600px]"
+          ref={mapRef}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -28,7 +78,8 @@ const Coverage = () => {
           {serviceCenters.map((center, index) => (
             <Marker key={index} position={[center.latitude, center.longitude]}>
               <Popup>
-                <strong>{center.district}</strong> <br /> {center.covered_area.join(', ')}
+                <strong>{center.district}</strong> <br />{" "}
+                {center.covered_area.join(", ")}
               </Popup>
             </Marker>
           ))}
